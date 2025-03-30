3f2a7c50232d678373da4bed03e124ecd3d02a00
Sbpena01 <sbpena01@gmail.com>
2025-03-30 19:51:23 -0400

    added YOLO model to look for traffic signs

--- ModelDetection.py
+++ ModelDetection.py
@@ -6,41 +6,41 @@ import torch
 
 from Detection import *
 
-def get_detections_from_image(image: np.ndarray, model: YOLO) -> list[Detection]:
+def get_detections_from_image(image: np.ndarray, models: list[YOLO]) -> list[Detection]:
     detections = []
-
-    results = model(image)
-    
-    for result in results:
-        for box in result.boxes:
-            class_id = model.names[int(box.cls)]
-            u1, v1, u2, v2 = box.xyxy.numpy().flatten()
-            confidence = box.conf.numpy().flatten()[0]
-            detection = Detection(class_id, confidence, np.array([u1, v1]), np.array([u2, v2]))
-            detections.append(detection)
-    
+    for model in models:
+        results = model(image)
+        
+        for result in results:
+            for box in result.boxes:
+                class_id = model.names[int(box.cls)]
+                u1, v1, u2, v2 = box.xyxy.numpy().flatten()
+                confidence = box.conf.numpy().flatten()[0]
+                detection = Detection(class_id, confidence, np.array([u1, v1]), np.array([u2, v2]))
+                detections.append(detection)
     return detections
 
 def main():
     # NOTE: Temporary code to test functions in this file. Much of this logic can be written in main.py
-    cap = cv2.VideoCapture("scene6_front.mp4") # scene 6 is a disaster...
+    cap = cv2.VideoCapture("Videos/scene10_front.mp4") # scene 6 is a disaster...
     # torch.hub.help("intel-isl/MiDaS", "DPT_BEiT_L_384", force_reload=True)
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    model = YOLO("yolov8n.pt", verbose=False)  # General YOLO Model for Vehicles, Traffic Lights, and Pedestrians
-    rand_start = random.randint(600,1000)
+    sign_model = YOLO("best.pt", verbose=False)  # General YOLO Model for Vehicles, Traffic Lights, and Pedestrians
+    general_model = YOLO("yolov8n.pt", verbose=False)
+    rand_start = random.randint(700,1000)
     counter = 0
     if not cap.isOpened():
         raise RuntimeError("Error: Could not open video file.")
     while True:
         # Read a frame from the video
         ret, frame = cap.read()
-        if not ret: # no more frames
+        if not ret: # no more framesq
             break
         # choosing a random frame in the video to begin analysis
         if counter < rand_start:
             counter+=1
             continue
-        detections = get_detections_from_image(frame, model)
+        detections = get_detections_from_image(frame, [general_model, sign_model])
         for detection in detections:
             print(f"Found {detection.class_id}")
             cv2.rectangle(frame, np.uint16(detection.top_left), np.uint16(detection.bottom_right), (255, 0, 0), 1)
